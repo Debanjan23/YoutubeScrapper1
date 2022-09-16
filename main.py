2085e66a978be5a346fa3ae02a6e74e0b1535120
Debanjan23 <debnusiet06@gmail.com>
2022-09-17 02:02:26 +0530

    comit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+import webbrowser
 from bs4 import BeautifulSoup as bs
 from flask import Flask, render_template, request
-from flask_cors import CORS,cross_origin
+from flask_cors import cross_origin
 import json
 import requests
 import re
 import pandas as ps
-import os
 import Data
-from Schema import Load_Data as ld
+#from Schema import Load_Data as ld
 
 app = Flask(__name__)
 
@@ -29,18 +29,24 @@ def main():
             Data_obj = Data.Data()
             p = Data_obj.get_video_stats(channel_id)
             df = ps.DataFrame(p)
-            filename = df['Youtuber_name'][0].replace(' ','_') + '.csv'
-            df.to_csv(filename, encoding='utf-8', index=False)
-            if filename!='':
-              os.system("csvsql --db mysql://root:Skhan87!@localhost:3306/test --insert --overwrite "+filename)
+            html = df.to_html()
+            text_file=open('result_data.html','w', encoding='utf-8')
+            text_file.write(html)
+            text_file.close()
+            #filename = df['Youtuber_name'][0].replace(' ','_') + '.csv'
+            #df.to_csv(filename, encoding='utf-8', index=False)
+            #if filename!='':
+              #os.system("csvsql --db mysql://root:Skhan87!@localhost:3306/test --insert --overwrite "+filename)
 
-            msg = ld.Load_Data.load_mongo(p)
-            if msg == 1:
-                return render_template('load_data.html')
-            else:
-                return 'Data not loaded'
+            #msg = ld.Load_Data.load_mongo(p)
+            #if msg == 1:
+            #return render_template('load_data.html')
+            return webbrowser.open('result_data.html')
+            #else:
+            #return 'Data not loaded'
         except Exception as e:
           print(e)
 
 if __name__=='__main__':
+    #app.run(host='127.0.0.1', port=8001, debug=True)
     app.run(debug=True)
